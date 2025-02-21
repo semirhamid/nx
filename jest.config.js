163c46b2312bb8a7e4b3cb60aee7feb8efe140c0
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2025-02-21 11:18:37 +0300

    Add project configuration files for development and deployment

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/app.js',
+    '!src/config/*.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+}; 
